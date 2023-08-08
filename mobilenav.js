9881b8c4e4e51dbcb9c60e6aa3663916f850589c
Lizdigital <140196374+Lizdigital@users.noreply.github.com>
2023-08-08 10:20:27 +0100

    Delete mobilenav.js

--- mobilenav.js
+++
@@ -1,6 +0,0 @@
-const mobileToggle = document.querySelector('.mobile-toggle');
-const mobileNav = document.querySelector('.mobile-nav');
-
-mobileToggle.addEventListener('click', () => {
-  mobileNav.classList.toggle('active');
-});
